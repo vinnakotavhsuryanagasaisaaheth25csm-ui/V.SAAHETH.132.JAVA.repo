3c1ba6ea86a3619270059c4bbc4fc539bfbfe507
vinnakotavhsuryanagasaisaaheth25csm-ui <vinnakotavhsuryanagasaisaaheth.25.csm@anits.edu.in>
2026-09-16 12:03:30 +0530

    Added Test2 class file

---
+++ Test2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Test2
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        if (scanner.hasNextInt()) {
+            int t = scanner.nextInt();
+            
+            
+            while (t-- > 0) {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                
+      
+                int totalCoins = x * y;
+                int maxBags = totalCoins / 100;
+                
+            
+                System.out.println(maxBags);
+            }
+        }
+        
+        scanner.close();
+    }
+}
